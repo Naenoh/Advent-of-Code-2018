4f1265ac91703370b77513e290f1a410e5438187
DIDES ARNAUD p1610215 <arnaud.dides@etu.univ-lyon1.fr>
2018-12-01 16:52:14 +0100

    Day one finished

--- src/dayOne.kt
+++ src/dayOne.kt
@@ -1,3 +1,24 @@
-fun firstPuzzle(freqs: Array<Int>): Int {
-    return(freqs.sum())
+import java.io.File
+
+fun firstPuzzle(freqs: Array<Int>) = freqs.sum()
+
+fun secondPuzzle(freqs: Array<Int>): Int {
+    var currentFreq = 0
+    val freqReachedList: MutableList<Int> = mutableListOf()
+    var i = 0
+    while (currentFreq !in freqReachedList) {
+        freqReachedList.add(currentFreq)
+        currentFreq += freqs[i]
+        i++
+        if (i == freqs.size) {
+            i = 0
+        }
+    }
+    return currentFreq
+}
+
+fun main(args: Array<String>) {
+    val file = File("inputs/dayOneFirstPuzzle.txt")
+    val numberArray = file.readLines().map(String::toInt).toTypedArray()
+    println(secondPuzzle(numberArray)) // 430
 }
--- test/DayOneTest.kt
+++ test/DayOneTest.kt
@@ -11,4 +11,13 @@ internal class DayOneTest {
         assertEquals(0, firstPuzzle(arrayOf(1, 1, -2)))
         assertEquals(-6, firstPuzzle(arrayOf(-1, -2, -3)))
     }
+
+    @Test
+    fun secondPuzzle() {
+        assertEquals(2, secondPuzzle(arrayOf(1, -2, 3, 1)))
+        assertEquals(0, secondPuzzle(arrayOf(1, -1)))
+        assertEquals(10, secondPuzzle(arrayOf(3, 3, 4, -2, -4)))
+        assertEquals(5, secondPuzzle(arrayOf(-6, 3, 8, 5, -6)))
+        assertEquals(14, secondPuzzle(arrayOf(7, 7, -2, -7, -4)))
+    }
 }
